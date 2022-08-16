a93615550e6fa9f1be12cde6e8f6c4adec27abcd
liruifengv <liruifeng1024@gmail.com>
2022-08-16 11:41:42 +0800

    style: font-family Raleway

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import '@fontsource/raleway'
+import '@fontsource/raleway/700.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
